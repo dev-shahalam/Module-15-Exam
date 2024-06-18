6d4964547beb5481774a9295c9370e43fb65b6b1
Md Shah Alam <mdshahalam8283@gmail.com>
2024-06-19 01:53:30 +0600

    Finish Module 15 assignment

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+  function index($id){
+      $name="Donal Trump";
+      $age=75;
+      $data=array(
+          "id"=>$id,
+          "name"=>$name,
+          "age"=>$age,
+      );
+      $name = 'access_token';
+      $value = '123-XYZ';
+      $minutes = '1';
+      $path = '/';
+      $domain = $_SERVER['SERVER_NAME'];
+      $secure = false;
+      $httpOnly = true;
+
+      return response($data,255)->cookie($name,$value,$minutes,$path,$domain,$secure,$httpOnly);
+
+
+  }
+}
